581ebabc8e9b0ecd51ea8f053f7c82ee5f6748ea
Ben Friedland <ben174@gmail.com>
2017-07-25 13:02:33 -0700

    Add dj-database-url

--- crime/settings.py
+++ crime/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
