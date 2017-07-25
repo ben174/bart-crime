d8047cc7c8f5fa4b976e4ba71b52b332724bc92f
Ben Friedland <ben174@gmail.com>
2017-07-25 13:33:16 -0700

    Disable csrf for webhook

--- reports/views.py
+++ reports/views.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from reports.models import Report
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def report_webhook(request):
     #TODO: authenticate via secret
     report = Report.objects.create(body=request.body)
