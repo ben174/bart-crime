25a436c109b03f3660915729a33eb8eaa8f5f5cc
Ben Friedland <ben174@gmail.com>
2017-07-25 12:50:47 -0700

    STATIC_ROOT

--- crime/settings.py
+++ crime/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
